d67c9332e9f572fcb841d2239ef160aa52bc86c5
lingjiahu <lingjiahu@outlook.com>
2022-02-14 21:07:46 -0500

    2 done

--- ass/a1/PG-260885791/pgsql/q02.sql
+++ ass/a1/PG-260885791/pgsql/q02.sql
@@ -1,1 +1,16 @@
--- Find the user id of all the users who have reviewed the movie (title) Casablanca. The output should be ordered by the user id. You may assume that there is only one movie with this title.
+-- list user id of all the users
+-- who have reviewed the movie (title) Casablanca
+-- ordered by the user id
+-- You may assume that there is only one movie with this title.
+
+WITH id(cid) AS
+(
+    SELECT movid
+    FROM movies
+    WHERE title = 'Casablanca'
+)
+SELECT R.userid
+FROM Review R, id
+WHERE R.movid = id.cid
+ORDER BY userid
+;
---
+++ ass/a1/PG-260885791/pgsql/q03.sql
@@ -0,0 +1,5 @@
+-- List the user id of all the users
+-- who gave a favourable review (rating 7 and above) for the 1959 version of the movie (title) Ben-Hur
+-- but did not review or gave an unfavourable review (rating 4 and below) for the 2016 version of the movie
+-- The output should be ordered by the user id.
+
