b1f0237e6f6318108cf82c4bdec99cf06ae5ae5a
lingjiahu <lingjiahu@outlook.com>
2022-02-15 15:49:06 -0500

    15 done

--- ass/a1/PG-260885791/pgsql/q15.sql
+++ ass/a1/PG-260885791/pgsql/q15.sql
@@ -3,3 +3,26 @@
 -- Movies with no reviews yet should not be ignored in the output.
 -- Name the number of reviews as numreviews.
 -- Order the output based on the title of the movie.
+
+WITH numreview(mid, num) AS
+         (
+             SELECT m1.movid, count(*) AS numreviews
+             FROM movies m1
+                      JOIN review r1 on m1.movid = r1.movid
+             GROUP BY m1.movid
+             UNION
+             SELECT m2.movid, 0 AS numreviews
+             FROM movies m2
+             WHERE m2.movid NOT IN (SELECT m3.movid
+                                    FROM movies m3
+                                             JOIN review r3 on m3.movid = r3.movid
+                                    GROUP BY m3.movid)
+         )
+SELECT M.title, NR.num AS numreviews
+FROM movies M
+         JOIN numreview NR ON M.movid = NR.mid
+WHERE M.releasedate = (SELECT MAX(M4.releasedate)
+                       FROM movies m4
+)
+ORDER BY M.title
+;
