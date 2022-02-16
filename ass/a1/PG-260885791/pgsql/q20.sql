89cdbf9ab15860a260746f417154c5689644c498
lingjiahu <lingjiahu@outlook.com>
2022-02-15 21:25:25 -0500

    20 done

--- ass/a1/PG-260885791/pgsql/q20.sql
+++ ass/a1/PG-260885791/pgsql/q20.sql
@@ -1,4 +1,4 @@
--- List all the languages in the database and the genre
+-- List all the languages and the genre
 -- that has most movies for that language.
 -- If two genres are equally the most popular for the language, produce two rows in the output (and so forth).
 -- Order the output by languages and then by genre.
@@ -11,3 +11,16 @@
 -- German | Drama
 -- Greek | Action
 -- Greek | Adventure
+
+WITH langen(lan, gen, cnt) AS
+    (SELECT L.language, G.genre, count(*)
+        FROM releaselanguages L JOIN moviegenres G ON L.movid = G.movid
+        GROUP BY L.language, G.genre)
+SELECT LG.lan, LG.gen
+FROM langen LG
+GROUP BY LG.lan, LG.gen, LG.cnt
+HAVING LG.cnt = (SELECT MAX(lg2.cnt)
+    FROM langen lg2 WHERE lg2.lan = LG.lan)
+ORDER BY LG.lan, LG.gen
+;
+
