aad33884121fd1d5b0eb58011d6f5c356b505ad3
lingjiahu <lingjiahu@outlook.com>
2022-02-22 21:45:58 -0500

    6

---
+++ project/p2/q6.sql
@@ -0,0 +1,17 @@
+-- Create a view midwifeinfo that shows the practitioner id, name, phone and email of all the midwives,
+-- the name of the clinic / birthing center that employs them, and the address of the facility.
+
+CREATE VIEW midwifeinfo(pid, mwname, mwtel, mwemail, hcname, hcaddress)
+AS
+SELECT mw.pid, mw.mwname, mwtel, mwemail, hc.hcname, hc.hcaddress
+FROM midwives mw
+         JOIN hcinstitutions hc on mw.hcid = hc.hcid
+;
+
+-- selects everything from the view, truncated to just 5 records
+SELECT * FROM midwifeinfo LIMIT 5;
+
+-- limits the previous output to only the midwives working at the facility whose name is Lac-Saint-Louis
+-- You can assume there is only one facility with that name.
+-- Truncate the output to just 5 records.
+SELECT * FROM midwifeinfo WHERE hcname = 'Lac-Saint-Louis' LIMIT 5;
