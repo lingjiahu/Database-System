461b4c38f6d2973dc749fb606d1643d13d731b08
lingjiahu <lingjiahu@outlook.com>
2022-02-23 19:52:55 -0500

    modified 5d

--- project/p2/5d.sql
+++ project/p2/5d.sql
@@ -1,16 +1,19 @@
 -- list the health care card, name and phone number of the mothers
--- who is currently pregnant and have not yet given birth and is under the care of a midwife employed by Lac- Saint-Louis.
+-- who is currently pregnant and have not yet given birth and is under the care of a midwife employed by Lac-Saint-Louis.
 -- you can assume that there is only one facility by that name
 
-WITH curpregs(cid, birthym) AS
-         (SELECT p.cid, p.birthym
+WITH curpregs(cid) AS
+         (SELECT p.cid
           FROM pregnancies p
-                   JOIN midwives mw ON p.ppid = mw.pid AND mw.hcid = 'Lac- Saint-Louis' AND
-                                       NOT EXISTS(SELECT b.bid
-                                                  FROM babies b
-                                                  WHERE b.cid = p.cid
-                                                    AND b.birthym = p.birthym
-                                                    AND b.bdob IS NULL)
+                   JOIN midwives mw ON p.ppid = mw.pid
+              AND mw.hcid IN (SELECT hc.hcid
+                              FROM hcinstitutions hc
+                              WHERE hc.hcname = 'Lac-Saint-Louis')
+              AND NOT EXISTS(SELECT b.bid
+                             FROM babies b
+                             WHERE b.cid = p.cid
+                               AND b.birthym = p.birthym
+                               AND b.bdob IS NOT NULL)
          ),
      curcouple(mramq) AS (SELECT c.mramq
                           FROM couples c
