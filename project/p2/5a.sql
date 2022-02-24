e67308ec0ebd2fd6834d3016f55c04eb7065bc7d
lingjiahu <lingjiahu@outlook.com>
2022-02-23 21:56:05 -0500

    5a done

--- project/p2/5a.sql
+++ project/p2/5a.sql
@@ -2,22 +2,24 @@
 -- for the midwife Marion Girard for the week March 21 - March 25 of this year (2022).
 -- You can assume that there is only one midwife with this particular name.
 
-WITH aptpreg(aid, cid, birthym) AS
+WITH aptpreg(aid, cid, birthym) AS -- all appointments with Marion Girard for March 21-25 in 2022
          (SELECT a1.aid, p.cid, p.birthym
           FROM pregnancies p
                    JOIN appointments a1 ON p.cid = a1.cid AND p.birthym = a1.birthym AND
                                            a1.pid IN (SELECT mw.pid
                                                       FROM midwives mw
                                                       WHERE mw.mwname = 'Marion Girard')
-              AND EXTRACT(year from a1.apptdate) = 2022 AND EXTRACT(month from a1.apptdate) = 03
-              AND EXTRACT(day from a1.apptdate) >= 21 AND EXTRACT(day from a1.apptdate) <= 25),
-     aptcouple(aid, ramq) AS
+              AND EXTRACT(YEAR FROM a1.apptdate) = 2022 AND EXTRACT(MONTH FROM a1.apptdate) = 03
+              AND EXTRACT(DAY FROM a1.apptdate) >= 21 AND EXTRACT(DAY FROM a1.apptdate) <= 25),
+     aptcouple(aid, ramq) AS -- all couples involved with the appointments
          (SELECT ap.aid, c.mramq
-             FROM couples c JOIN aptpreg ap ON c.cid = ap.cid),
-     aptmother(aid, ramq, manme, mtel) AS
+          FROM couples c
+                   JOIN aptpreg ap ON c.cid = ap.cid),
+     aptmother(aid, ramq, manme, mtel) AS -- all mothers involved with the appointments
          (
              SELECT ac.aid, m.mramq, m.mname, m.mtel
-             FROM mothers m JOIN aptcouple ac ON m.mramq = ac.ramq
+             FROM mothers m
+                      JOIN aptcouple ac ON m.mramq = ac.ramq
          )
 SELECT a.apptdate AS date, a.apptime AS time, am.ramq, am.manme AS name, am.mtel AS phone
 FROM appointments a
--- project/p2/droptbl.sql
+++ project/p2/droptbl.sql
@@ -7,20 +7,20 @@
 -- Remember to put the drop table ddls for the tables with foreign key references
 --    ONLY AFTER the parent tables has already been dropped (reverse of the creation order).
 
-DROP TABLE MOTHERS;
-DROP TABLE FATHERS;
-DROP TABLE COUPLES;
-DROP TABLE HCINSTITUTIONS;
-DROP TABLE COMMUNITYCLINICS;
-DROP TABLE BIRTHINGCENTERS;
-DROP TABLE PREGNANCIES;
-DROP TABLE INVITATIONS;
-DROP TABLE MIDWIVES;
-DROP TABLE INFOSESSIONS;
-DROP TABLE REGISTRATIONS;
-DROP TABLE BABIES;
-DROP TABLE APPOINTMENTS;
-DROP TABLE NOTES;
-DROP TABLE TECHNICIANS;
-DROP TABLE TESTS;
+DROP TABLE mothers;
+DROP TABLE fathers;
+DROP TABLE couples;
+DROP TABLE hcinstitutions;
+DROP TABLE communityclinics;
+DROP TABLE birthingcenters;
+DROP TABLE pregnancies;
+DROP TABLE invitations;
+DROP TABLE midwives;
+DROP TABLE infosessions;
+DROP TABLE registrations;
+DROP TABLE babies;
+DROP TABLE appointments;
+DROP TABLE notes;
+DROP TABLE technicians;
+DROP TABLE tests;
 DROP VIEW midwifeinfo;
